95e016c9c7eb1abde0b1d0996645bfb9ea4e7313
RanolP <public.ranolp@gmail.com>
2022-10-31 12:51:50 +0900

    index.html update

--- src/renderer/_default.page.server.tsx
+++ src/renderer/_default.page.server.tsx
@@ -16,16 +16,17 @@ export async function render(pageContext: PageContextServer) {
 
   // See https://vite-plugin-ssr.com/head
   const { documentProps } = pageContext.exports;
-  const title = (documentProps && documentProps.title) || 'Vite SSR app';
+  const title = (documentProps && documentProps.title) || 'Translyrics';
   const desc =
     (documentProps && documentProps.description) ||
-    'App using Vite + vite-plugin-ssr';
+    '의미는 그대로 박자와 운율은 맞춰서 번역한 가사들';
 
-  const documentHtml = escapeInject`<!DOCTYPE html>
-    <html lang="en">
+  const documentHtml = escapeInject`
+    <!DOCTYPE html>
+    <html lang="ko">
       <head>
         <meta charset="UTF-8" />
-        <link rel="icon" href="" />
+        <link rel="icon" href="TODO" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="${desc}" />
         <title>${title}</title>
@@ -33,7 +34,8 @@ export async function render(pageContext: PageContextServer) {
       <body>
         <div id="page-view">${dangerouslySkipEscape(pageHtml)}</div>
       </body>
-    </html>`;
+    </html>
+  `;
 
   return {
     documentHtml,
